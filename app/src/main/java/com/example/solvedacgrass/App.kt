82fcdd5b7989b5b9bb6e13ae1ae3ebffb7d81787
Factoriall <skarlgus0910@gmail.com>
2023-04-15 00:50:14 +0900

    힐트 추가

---
+++ app/src/main/java/com/example/solvedacgrass/App.kt
@@ -0,0 +1,11 @@
+package com.example.solvedacgrass
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * 클래스에 대한 간단한 설명이나 참고 url을 남겨주세요.
+ * Created by fac.toriall on 2023.04.14..
+ */
+@HiltAndroidApp
+class App : Application()
